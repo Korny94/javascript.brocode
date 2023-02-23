5afc72d676618711a6fab5bf6eed74b3909cee1b
Korny94 <karlmagnusnokling@hotmail.no>
2023-02-23 18:09:19 +0100

    update

---
+++ js/games.js
@@ -0,0 +1,86 @@
+// API CALL & GET QUERYSTRING
+
+
+const key = "?key=46351883e2e9453a8a40fc6b6cb983ea";
+
+const apiCall = document.querySelector("#apiCall");
+
+const gameDetails = document.querySelector("#gameDetails");
+
+const gameTitle = document.querySelector("#gameTitle");
+
+const queryString1 = document.location.search;
+
+const params1 = new URLSearchParams(queryString1);
+
+const id1 = params1.get("id");
+
+const url10 = "https://api.rawg.io/api/games" + key;
+
+console.log(id1);
+
+
+
+async function createGameList() {
+    try {
+        const response = await fetch(url10);
+        const json = await response.json();
+
+        console.log(json);
+
+        apiCall.innerHTML = "";
+
+        const games = json.results;
+
+        console.log(games)
+
+        games.forEach(function(game) {
+            apiCall.innerHTML += `
+                <a href="../game.html?id=${game.id}" class="gameCard">
+                    <div class="gameBgImg" style="background-image: url(${game.background_image})"></div>
+                    <h4>${game.name}</h4>
+                </a>
+            `
+        })
+
+    } catch(err) {
+        console.log("ERROR: ", err)
+    }
+}
+
+createGameList();
+
+
+const url11 = "https://api.rawg.io/api/games/" + id1 + key;
+
+async function fetchGame() {
+    try {
+        const response = await fetch(url11);
+        const details = await response.json();
+
+        const games = details.results;
+
+        console.log(games)
+
+        console.log(details)
+
+        createGameHtml(details);
+
+        gameTitle.innerHTML = details.name;
+
+    } catch(err) {
+        console.log("Error");
+    }
+}
+
+fetchGame();
+
+function createGameHtml(details) {
+    gameDetails.innerHTML = `
+        <h1>${details.name}</h1>
+        <div class="detailGameBgImg" style="background-image: url(${details.background_image})"></div>
+        <p>${details.description}</p>
+        <time>Released: ${details.released}</time>
+    `;
+
+}
--- js/script.js
+++ js/script.js
@@ -1587,6 +1587,9 @@ import teams from "./arrays.js";
 let teamsToRender = teams;
 
 function renderTeams() {
+
+    teamContainer.innerHTML = "";
+
     teamsToRender.forEach(function(team) {
         teamContainer.innerHTML += `
             <div class="team">
@@ -1598,8 +1601,21 @@ function renderTeams() {
 
 renderTeams();
 
-search.onkeyup = function() {
-    console.log(event);
+search.onkeyup = function(event) {
+    // console.log(event);
+    const searchValue = event.target.value.trim().toLowerCase();
+
+    const filteredTeams = teams.filter(function(team) {
+        if (team.full_name.toLowerCase().startsWith(searchValue)) {
+            return true;
+        }
+    })
+
+    // console.log(filteredTeams)
+
+    teamsToRender = filteredTeams;
+
+    renderTeams();
 }
 
 
